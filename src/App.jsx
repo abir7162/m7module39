24aadfa25c2684339e3453a2a750962acc351449
Your Name <www.mdabirahmedsl7162@gmail.com>
2023-09-12 02:27:06 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Counter from './counter'
-import Team from './team'
-import Users from './user'
-import Friends from './Friends'
-import Friend from './Friend'
+import Countries from './components_countries/Countries'
+
 
 function App() {
 
-  function handleClick(){
-    alert('button clicked');
-  }
-  const handleClick2 = () =>{
-    alert('clicked 2')
-  }
-  const handleFour = (num) =>{
-    alert(num + 5)
-  }
+ 
  return(
 
 <>
 
-<h3>React Core concep 2</h3>
+<h3>React world tour</h3>
+<Countries></Countries>
 
-<Friends></Friends>
-<Users></Users>
-<Team></Team>
-<Counter></Counter>
-<button onClick={handleClick}>Click me</button>
-<button onClick={handleClick2}>Button click 2</button>
-<button onClick={()=>{alert('clicked 3')}}>button 3</button>
-<button onClick={() =>{handleFour(5)}}>button4</button>
+
 </>
 
  )
